48a437c2cf660786c1540a26bb7eecf601ce73c7
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2024-08-18 17:16:46 +0530

    Update day_duration.R
    Fixed an error in the day_duration.R file

--- R/day_duration.R
+++ R/day_duration.R
@@ -13,7 +13,7 @@
 #' day_duration(swephR::swe_julday(2022,7,14,0,swephR::SE$GREG_CAL),c(15.34, 75.13, +5.5))
 day_duration <- function(jd,place){
   srise = sunrise(jd,place)[1]
-  sset = sunrise(jd,place)[1]
+  sset = sunset(jd,place)[1]
   diff = (sset - srise) * 24
   return (c(diff,to_dms(diff)))
 }
